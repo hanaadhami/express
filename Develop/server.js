874940317d711b7712702a8013b7526c8a6e78be
Hana <hanaadhami01@gmail.com>
2019-12-10 17:04:24 -0800

    changing port to 3000

--- Develop/server.js
+++ Develop/server.js
@@ -7,8 +7,8 @@ const store = new Store({file: "db.json"});
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8000;
-var PORT = process.env.PORT || 8000;
+var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
